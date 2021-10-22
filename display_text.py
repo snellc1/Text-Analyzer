36c86e435728852b621892559cc26dbffb92fb64
Christopher Snell <chris.snell2019@gmail.com>
2021-10-22 10:47:52 -0700

    Display text box and created variable.

--- display_text.py
+++ display_text.py
@@ -1,1 +1,15 @@
-import tkinter
+from tkinter import * 
+
+text_anaylzer = Tk()
+
+#App Title
+text_anaylzer.title("Text Analyzer")
+
+#Setting the window size
+text_anaylzer.geometry("600x400")
+
+Label(text_anaylzer, text="Please enter text").grid(row=0)
+
+entry = Entry(text_anaylzer)
+
+text_anaylzer.mainloop()
