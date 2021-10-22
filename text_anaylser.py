60be48151111c06a9fd2728481cdb5fd8ee92595
Christopher Snell <chris.snell2019@gmail.com>
2021-10-22 14:07:27 -0700

    Added character and vowel onto widget.

--- text_anaylser.py
+++ text_anaylser.py
@@ -5,18 +5,22 @@ text_anaylzer = Tk()
 text_anaylzer.title("Text Analyzer")
 
 #Setting the window size
-text_anaylzer.geometry("600x150")
+text_anaylzer.geometry("600x400")
 
 #Declaring string variable for provide text
 input_text = StringVar()
 
-#Creating entry label
-entry_lb = Label(text_anaylzer, text= "Please provide text:")
-word_label = Label(text_anaylzer, text="Word Count: ")
+#Creating entry labels
+entry_lb = Label(text_anaylzer, text= "Please provide text")
+word_label = Label(text_anaylzer, text="Word Count")
+char_label = Label(text_anaylzer, text="Character Count")
+vowel_label = Label(text_anaylzer, text="Vowel Count")
 
-#Creating Entry
+#Creating entries
 entry = Entry(text_anaylzer, textvariable=input_text, width=50, font=('calibre', 10, 'normal'))
 word_entry = Entry(text_anaylzer)
+char_entry = Entry(text_anaylzer)
+vowel_entry = Entry(text_anaylzer)
 
 # Function to get text and determine word, character, vowel and punctuation count.
 def get_text():
@@ -43,7 +47,10 @@ def char_count(text):
             chars.append(c)
     
     total_char = len(chars)
-    print(f'Character Count: {total_char}')
+    
+    #Display character count
+    char_entry.insert(0, f"{total_char}")
+
 
 def vowel_count(text):
     """Count the total amount of vowels in provide text."""
@@ -53,8 +60,8 @@ def vowel_count(text):
         if l.lower() in vowels:
             total_vowels += 1
        
-    
-    print(f'Vowel Count: {total_vowels}')
+    #Display vowel count
+    vowel_entry.insert(0, f"{total_vowels}")
 
 def pun_count(text):
     """Count the total amount of punctuations in provide text."""
@@ -69,12 +76,16 @@ def pun_count(text):
 #Creating Anaylzer Button
 anaylze_button = Button(text_anaylzer, text="Anaylze", command= get_text)
 
-#Placing label and entry 
+#Placing labels and entries 
 entry_lb.pack()
 entry.pack()
 anaylze_button.pack()
 word_label.pack()
 word_entry.pack()
+char_label.pack()
+char_entry.pack()
+vowel_label.pack()
+vowel_entry.pack()
 
 #Padding
 
